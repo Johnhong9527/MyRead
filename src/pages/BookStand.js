152da4c1f19f36560a7e1ce0cc273d67552e0214
seam <honghaitzz11@gmail.com>
2017-08-02 14:15:13 +0800

    0

--- src/pages/BookStand.js
+++ src/pages/BookStand.js
@@ -2,17 +2,50 @@ import React from 'react';
 import {
   Text,
   View,
+  Modal,
+  FlatList,
+  TouchableHighlight,
   StyleSheet
 } from 'react-native';
 import axios from 'axios'
-
 export default class BookStand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      chapterBody: '',
+      chapterTitle: ''
+    };
+  }
+
+  componentWillMount() {
+    axios.get('http://chapterup.zhuishushenqi.com/chapter/http://read.shuhaha.com/Html/Book/0/122/4199572.shtml').then(res=>{
+      // console.log(res.data.chapter.body.replace('↵','\n').split('\n'));
+      // console.log(res.data.chapter.body.split('\n'))
+      let chapterBody = res.data.chapter.body.replace('↵','\n').split('\n')
+      for (let i in chapterBody) {
+        chapterBody[i] = {
+            name: '       ' + chapterBody[i]
+        }
+      }
+      this.setState({
+        chapterBody: chapterBody,
+        chapterTitle:res.data.chapter.title
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.chapterBody)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          BookStand
-        </Text>
+        <Text style={styles.welcome}>{this.state.chapterTitle}</Text>
+        <FlatList
+          data={this.state.chapterBody}
+          horizontal={false}
+          keyExtractor={(item) => item.name}
+          renderItem={({item}) => <Text style={styles.textItem}>{item.name}</Text>}
+        />
       </View>
     );
   }
@@ -23,9 +56,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#dad9d4',
   },
   welcome: {
-    fontSize: 25
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#000',
+    borderBottomWidth: 1
+  },
+  textItem: {
+    color: '#000',
+    margin: 5,
+    lineHeight: 30
   }
 });
